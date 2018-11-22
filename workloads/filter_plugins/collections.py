c3d49ff519d034a20a94473ec587c7e8c7b715da
Bugaj, Szymon <szymon.bugaj@intel.com>
2018-11-22 10:21:29 +0100

    Add full support for workloads versions and components count.

--- workloads/filter_plugins/collections.py
+++ workloads/filter_plugins/collections.py
@@ -1,24 +1,14 @@
-def instances_version_iter(workloads_versions):
+def instances_version_iter(workloads_versions, job_id):
     '''
     returns list to be ready to be iterated by
     '''
     r = []
     i = 0
     for wvn, wv in workloads_versions.items():
-        for i_ in range(wv['count']):
-            r.append((i, wvn))
-            i+=1
-    return r
-
-
-def instances_version_iter_2(workloads_versions, job_id):
-    '''
-    returns list to be ready to be iterated by
-    '''
-    r = []
-    i = 0
-    for wvn, wv in workloads_versions.items():
-        unique_count = wv[job_id]['unique_count']
+        try:
+            unique_count = wv[job_id]['unique_count']
+        except Exception:
+            unique_count = 1
         for i_ in range(wv['count']):
             for i__ in range(unique_count):
                 r.append((i, wvn, i__))
@@ -31,5 +21,4 @@ class FilterModule(object):
     def filters(self):
         return {
             'instances_version_iter': instances_version_iter,
-            'instances_version_iter_2': instances_version_iter_2
         }
